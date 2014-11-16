defd10eac312e998c8e8baabccf50c07826fd767
hnamkoong <hnamkoong1@gmail.com>
2014-11-17 00:23:22 +0900

    fixture update

--- cocktailor/auth/views.py
+++ cocktailor/auth/views.py
@@ -7,6 +7,9 @@ Created on 2014. 11. 16.
 from flask import Blueprint, flash, redirect, url_for, request, current_app
 auth = Blueprint("auth", __name__)
 
+from flask.ext.login import (current_user, login_user, login_required,
+                             logout_user, confirm_login, login_fresh)
+
 from cocktailor.auth.forms import LoginForm
 from cocktailor.auth.models import User
 from cocktailor.utils.helpers import render_template
@@ -16,19 +19,19 @@ def login():
     """
     Logs the user in
     """
- 
-#     form = LoginForm(request.form)
-#     if form.validate_on_submit():
-#         user, authenticated = User.authenticate(form.login.data,
-#                                                 form.password.data)
-#  
+
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user, authenticated = User.authenticate(form.login.data,
+                                                form.password.data)
+
+        print(user)
+        print(authenticated)
+        
 #         if user and authenticated:
 #             login_user(user, remember=form.remember_me.data)
 #             return redirect(request.args.get("next") or
 #                             url_for("forum.index"))
-#  
 #         flash(("Wrong username or password"), "danger")
-#         
-    error = None
-    form = LoginForm(request.form)
+
     return render_template("auth/login.html", form=form)
--- cocktailor/fixture/menus.py
+++ cocktailor/fixture/menus.py
@@ -24,6 +24,24 @@ fixture = [
         'name' : 'Hurricane',
         'description' : 'Break away from the basic cocktail for a minute, as this favorite has an extensive ingredient list.',
         'price' : 11,
-        'category_id' : 1,
+        'category_id' : 2,
+    },
+    {
+        'name' : 'Daiquiri',
+        'description' : 'The Daiquiri is the first rum drink everyone should learn how to make and that doesn\'t mean finding the best way to open the ready-to-drink Daiquiri bottle or pouch.',
+        'price' : 10,
+        'category_id' : 3,
+    },
+    {
+        'name' : 'Mojito',
+        'description' : 'When you have the Daiquiri perfected, it is time to try out the Mojito.',
+        'price' : 13,
+        'category_id' : 4,
+    },
+    {
+        'name' : 'Hurricane',
+        'description' : 'Break away from the basic cocktail for a minute, as this favorite has an extensive ingredient list.',
+        'price' : 11,
+        'category_id' : 5,
     },
 ]

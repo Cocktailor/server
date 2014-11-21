cadf93c06ad054f93a5aff07e679be474409a124
Taesoo Lee <rhee92@kaist.ac.kr>
2014-11-22 04:52:56 +0900

    image all

--- manage.py
+++ manage.py
@@ -41,8 +41,8 @@ import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # Run local server
-# manager.add_command("runserver", Server("localhost", port=4418))
-manager.add_command("runserver", Server("cs408.kaist.ac.kr", port=4418))
+manager.add_command("runserver", Server("localhost", port=4418))
+# manager.add_command("runserver", Server("cs408.kaist.ac.kr", port=4418))
 
 
 @manager.command

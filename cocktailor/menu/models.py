0f67313ed6e8e3407df869742221ed851e47e23c
Taesoo Lee <rhee92@kaist.ac.kr>
2014-11-22 01:09:11 +0900

    taesoo view work

--- cocktailor/menu/models.py
+++ cocktailor/menu/models.py
@@ -13,7 +13,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     description = db.Column(db.String(200))
-    restaurant_id = db.Column(db.Integer)
+#     restaurant_id = db.Column(db.Integer)
 
     # Methods
     def __repr__(self):
@@ -39,8 +39,16 @@ class Category(db.Model):
         values['id'] = self.id
         values['name'] = self.name
         values['description'] = self.description
-        values['restaurant_id'] = self.restaurant_id
+#         values['restaurant_id'] = self.restaurant_id
         return values
+    
+    def insert_name(self,name):
+        self.name = name
+        return self.name
+    
+    def insert_description(self,des):
+        self.description = des
+        return self.description
     
 
 class Menu(db.Model):
@@ -83,4 +91,20 @@ class Menu(db.Model):
         values['picture'] = self.picture
         values['category_id'] = self.category_id
         return values
+    
+    def insert_name(self,name):
+        self.name = name
+        return self.name
+    
+    def insert_description(self,des):
+        self.description = des
+        return self.description
+    
+    def insert_price(self,price):
+        self.price = price
+        return self.price
+    
+    def insert_category_id(self,c_id):
+        self.category_id = c_id
+        return self.category_id
     
--- cocktailor/menu/views.py
+++ cocktailor/menu/views.py
@@ -28,3 +28,66 @@ def index():
         MenusArray.append(m.values())
         
     return render_template("menu/index.html", categories=CategoriesArray, menus=MenusArray)
+
+@menu.route("/edit", methods=['GET', 'POST'])
+def edit():
+    categories = Category.query.all()
+    CategoriesArray = []
+    for c in categories:
+        CategoriesArray.append(c.values())
+    menus = Menu.query.all()
+    MenusArray = []
+    for m in menus:
+        MenusArray.append(m.values())
+        
+    return render_template("menu/edit.html", categories=CategoriesArray, menus=MenusArray)
+
+@menu.route("/edit/new_category", methods=["POST", "GET"])
+def new_category():
+    if request.method == "POST":
+        name = request.form['cname']
+        desc = request.form['cdesc']
+        ctgr = Category()
+        ctgr.insert_name(name)
+        ctgr.insert_description(desc)
+        ctgr.save()
+        return redirect(url_for('menu.edit'))
+    return render_template("menu/new_category.html")
+
+@menu.route("/edit/<int:c_id>/new_menu", methods=["POST", "GET"])
+def new_menu(c_id):
+    if request.method == "POST":
+        name = request.form['mname']
+        desc = request.form['mdesc']
+        price = request.form['mprice']
+        menu = Menu()
+        menu.insert_name(name)
+        menu.insert_price(price)
+        menu.insert_description(desc)
+        menu.insert_category_id(c_id)
+        menu.save()
+        return redirect(url_for('menu.edit'))
+    return render_template("menu/new_menu.html")
+
+@menu.route("/edit/<int:c_id>/del_category", methods=["POST", "GET"])
+def del_category(c_id):
+    categories = Category.query.all()
+    menus = Menu.query.all()
+    for m in menus:
+        if m.category_id == c_id:
+            m.delete()
+    for c in categories:
+        if c.id == c_id:
+            c.delete()
+            break
+    return redirect(url_for('menu.edit'))
+
+@menu.route("/edit/<int:m_id>/del_menu", methods=["POST", "GET"])
+def del_menu(m_id):
+    menus = Menu.query.all()
+    for m in menus:
+        if m.id == m_id:
+            m.delete()
+            break
+    return redirect(url_for('menu.edit'))
+
